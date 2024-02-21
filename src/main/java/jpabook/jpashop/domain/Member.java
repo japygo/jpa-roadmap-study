100f45c3b611ab7bd7f7d240f8230157262f5179
japygo <hyunnjinn@gmail.com>
2024-02-21 17:03:59 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -16,6 +20,9 @@ public class Member {
     private String city;
     private String street;
     private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
         return id;
