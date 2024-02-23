f12e44985d1c2607af449a64ef28b62d2ec78ac2
japygo <hyunnjinn@gmail.com>
2024-02-23 17:03:47 +0900

    경로 표현식

---
+++ src/main/java/jpql/JpaMain7.java
@@ -0,0 +1,58 @@
+package jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain7 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("관리자");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+            member.changeTeam(team);
+            em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+//            String query = "select m.username from Member m";
+//            String query = "select m.team.name from Member m";
+//            List<String> resultList = em.createQuery(query, String.class).getResultList();
+            String query = "select t.members from Team t";
+            List resultList = em.createQuery(query, List.class).getResultList();
+
+            for (Object s : resultList) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
