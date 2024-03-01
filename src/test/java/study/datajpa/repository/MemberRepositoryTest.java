5b231cb796003725e90b505cc569e596cdfcecd8
japygo <hyunnjinn@gmail.com>
2024-03-01 17:07:08 +0900

    네이티브 쿼리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -427,4 +427,34 @@ class MemberRepositoryTest {
             System.out.println("nestedClosedProjections.teamName = " + nestedClosedProjections.getTeam().getName());
         }
     }
+
+    @Test
+    void nativeQuery() {
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+        Member result = memberRepository.findByNativeQuery("m1");
+
+        // then
+        System.out.println("result = " + result);
+
+        // when
+        Page<MemberProjection> content = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+
+        // then
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection = " + memberProjection.getUsername());
+            System.out.println("memberProjection = " + memberProjection.getTeamName());
+        }
+    }
 }
